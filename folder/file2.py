b3fb72fad02911986a4ac5c01754942a24e3e393
jan <developerbase2@gmail.com>
2023-12-02 17:57:17 +0100

    commit 1

--- folder/file2.py
+++ folder/file2.py
@@ -4,3 +4,7 @@ print('Hello, world!')
 print('And another change, but in different branch!')
 print('Adding a new print!')
 print('Adding a new print4!')
+
+# This program prints Hello, world!
+
+print('feature-3')
