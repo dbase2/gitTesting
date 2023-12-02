7b9ce91aeb01d0fceb04f71bb75cae586a6f17de
jan <developerbase2@gmail.com>
2023-12-02 17:31:31 +0100

    test

--- folder/file2.py
+++ folder/file2.py
@@ -1,6 +1,5 @@
 # This program prints Hello, world!
 
 print('Hello, world!')
-print('And another change, but in different branch!')
 print('Adding a new print!')
-print('Adding a new print4!')
+print('Adding a new print2!')
